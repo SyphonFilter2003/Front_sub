4a0ce1045a46f8ae6e62d03705c913fddb9c2af4
Joao <joaoantoniowb2003@gmail.com>
2024-12-11 00:25:47 -0300

    comentários removidos

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Criando o root da aplicação com 'createRoot' (nova abordagem do React 18)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Renderizando o componente 'App' dentro do root
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// Passando o log das métricas de desempenho
 reportWebVitals(console.log);
